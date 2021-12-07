4f337f2231d55200393b017338a5a4590b066192
Sundeberg <Soendberg1994@gmail.com>
2021-12-07 14:08:26 +0100

    Edited getMatch in MatchDAO

--- src/dao/MatchDAO.java
+++ src/dao/MatchDAO.java
@@ -5,21 +5,26 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.LinkedList;
+import java.util.List;
 
 import model.Match;
+import model.MatchRoundResult;
 import model.Team;
 
 
 public class MatchDAO implements MatchDAOIF{
 	private DbConnectionIF dbConnection;
-	private MatchRoundResultDAO matchRoundResultDAO;
+
 	
 	public MatchDAO() {
 		
 	}
 	
 	public void setMatchRoundResult(int matchRoundResultId ,Team winningTeam) throws SQLException {
+		MatchRoundResultDAOIF matchRoundResultDAO = DAOFactory.createMatchRoundResultDAO(dbConnection);
 		matchRoundResultDAO.setWinner(matchRoundResultId, winningTeam);
+		
 	}
 	
 	public void setIsDraw() {
@@ -35,6 +40,8 @@ public class MatchDAO implements MatchDAOIF{
 		
 		Match foundMatch = null;
 		
+		MatchRoundResultDAOIF matchRoundResultDAO = DAOFactory.createMatchRoundResultDAO(dbConnection);
+		
 		try {
 			Connection connection = dbConnection.getConnection();
 			PreparedStatement statement = connection.prepareStatement(sqlQuery);
@@ -43,15 +50,22 @@ public class MatchDAO implements MatchDAOIF{
 			
 			ResultSet rs = statement.executeQuery();
 			
-			while (rs.next()) {
-				Team team = new Team(rs.getInt(3), rs.getString(4));
-				foundMatch = new Match(null, null);
-			}
+			List<MatchRoundResult> matchRoundResults = new LinkedList<>();
+			List<Team> listOfTeams = new LinkedList<>();
 			
+			while (rs.next()) {
+				matchRoundResults.add(matchRoundResultDAO.getRoundResult(rs.getInt(2)));
+				Team team = new Team(rs.getInt(3), rs.getString(4));
+				if(listOfTeams.isEmpty() || listOfTeams.contains(team)) {
+					listOfTeams.add(team);
+				}
+				
+			}
+			foundMatch = new Match(matchRoundResults, listOfTeams);
 		} catch (Exception e) {
 			// TODO: handle exception
 		}
-		return null;
+		return foundMatch;
 	}
 	
 	public void createRoundResult() {
